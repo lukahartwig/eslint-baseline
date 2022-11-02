58fcee8be37457056dbd905c7af7f85058ae82ac
Luka Hartwig <mail@lukahartwig.de>
2022-11-03 00:14:28 +0100

    chore: add more eslint plugins and fix errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,30 @@
+const jestPackageJson = require("jest/package.json");
+
 module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:jest/recommended",
-    "prettier",
+  extends: ["eslint:recommended", "plugin:node/recommended", "prettier"],
+  overrides: [
+    {
+      files: ["*.ts", "*.mts", "*.cts", "*.tsx"],
+      extends: [
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:jest/recommended",
+      ],
+    },
   ],
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.eslint.json"],
+  },
   plugins: ["@typescript-eslint"],
   root: true,
   settings: {
     jest: {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      version: require("jest/package.json").version,
+      version: jestPackageJson.version,
     },
   },
 };
